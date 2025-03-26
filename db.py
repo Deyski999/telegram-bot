4718bf9d11d0fc7ca0bcb9f02036befc2d7495a4
Ghaith Alrahi <ghaith.alrahi@gmail.com>
2025-03-26 23:16:30 +0200

    Final fix for MongoDB TLS

--- db.py
+++ db.py
@@ -3,6 +3,12 @@ import certifi
 
 MONGODB_URI = "mongodb+srv://ghaithalrahi:Searchersof99!@deyski999.j1l0sp5.mongodb.net/?retryWrites=true&w=majority"
 
-client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
+client = MongoClient(
+    MONGODB_URI,
+    tls=True,
+    tlsAllowInvalidCertificates=True,
+    tlsCAFile=certifi.where()
+)
+
 db = client["telegram_bot"]
 users_collection = db["users"]
