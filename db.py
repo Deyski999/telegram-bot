b21f6761f50917fcea1a029401936b89f15b7c92
Ghaith Alrahi <ghaith.alrahi@gmail.com>
2025-03-26 23:04:44 +0200

    Fix TLS handshake with MongoDB Atlas

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
+import certifi
 
 MONGODB_URI = "mongodb+srv://ghaithalrahi:Searchersof99!@deyski999.j1l0sp5.mongodb.net/?retryWrites=true&w=majority"
 
-client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
-
+client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
 db = client["telegram_bot"]
 users_collection = db["users"]
